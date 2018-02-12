0306b8968af54879cb6766ce9ccde83a05b5667c
Stephen McLaughlin <mclaughsj2@gmail.com>
2018-02-12 01:16:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,5 +21,8 @@ function sayHiToGrandma(string) {
   else if (string.toLowerCase() === string) {
     return "I can't hear you!";
   }
+  else if (string === "I love you, Grandma.") {
+    return "I love you, too.";
+  }
 }
 
